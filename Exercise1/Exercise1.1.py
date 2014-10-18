6fec23de86a96974120ef0cf19d88cb6c00da830
Harmenius <Harmenius@gmail.com>
2014-10-18 23:08:50 +0200

    Calculations up to mu_p

--- Exercise1/Exercise1.1.py
+++ Exercise1/Exercise1.1.py
@@ -1,15 +1,19 @@
 __author__ = 'Harmen'
 
 import numpy as np
-name = {}
 
-mu_tilde = np.array([1, 0, 1, 2]).T
+
+x_b = np.array([0,0])
+
+mu = np.array([1, 0, 1, 2]).T
 Sigma_tilde = np.array([ [ 0.14,-0.30, 0.00, 0.20]
                         ,[-0.30, 1.16, 0.20,-0.80]
                         ,[ 0.00, 0.20, 1.00, 1.00]
                         ,[ 0.20,-0.80, 1.00, 2.00]])
 Lambda_tilde = np.linalg.inv(Sigma_tilde)
+Lambda = {'aa':Lambda_tilde[0:2,0:2], 'ab':Lambda_tilde[0:2,2:4],
+          'ba':Lambda_tilde[2:4,0:2], 'bb':Lambda_tilde[2:4,2:4]}
 Sigma_p = np.linalg.inv(Lambda_tilde[0:2,0:2])
+mu_p = Sigma_p.dot(Lambda['aa'].dot(mu[:2]) - Lambda['ab'].dot(x_b - mu[2:]))
 
-
-print Sigma_p
+print mu_p
